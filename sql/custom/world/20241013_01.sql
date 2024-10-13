41d675e58db11d987ab5f09d9acd11141d83a678
Alyst3r <59404251+Alyst3r@users.noreply.github.com>
2024-10-13 19:30:36 +0200

    Sql tweaks

---
+++ sql/custom/world/20241013_01.sql
@@ -0,0 +1,3 @@
+DELETE FROM spell_learn_spell WHERE entry = 58984;
+DELETE FROM spell_pet_auras WHERE pet IN (416, 417, 1860, 1863, 17252);
+DELETE FROM spell_script_names WHERE ScriptName = 'spell_warr_second_wind';
